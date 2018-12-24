3ce731f29979087e6acb7d2823585de756654342
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2018-12-24 11:34:19 +0000

    adding yargs modules and config files

--- sf_subscriber.js
+++ sf_subscriber.js
@@ -1,56 +1,64 @@
-var faye = require('faye'),
-    request = require('request'),
-	unescapeJs = require('unescape-js');
+'use strict';
 
-//var token, instance_url;
-
-var url_post = {
-    url: 'https://test.salesforce.com/services/oauth2/token',
-    form: {
-        client_id: 'client_id',
-        client_secret: 'client_secret',
-        username: 'username',
-        password: 'password',
-        grant_type: 'password'
-    }
-};
+const faye = require( 'faye' ),
+    request = require( 'request' ),
+	unescapeJs = require( 'unescape-js' ),
+	args = require( 'yargs' ).argv,
+	config = require('./config');
 
 // this is going to be our callback function after we authenticate to the a particular server
 function getOAuthAndListenForEvents(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        let info = JSON.parse(body);
+
+    if ( !error && response.statusCode == 200 ) {
+        let info = JSON.parse( body );
 
         let access_token = info.access_token;
         let instance_url = info.instance_url;
+		let sf_event_name = args.event;
 
         //console.log('token: ' + access_token);
-        console.log('instance_url: ' + instance_url);
+        console.log( 'instance_url: ' + instance_url );
+        console.log( 'sf_event_name: ' + sf_event_name );
         //console.log('token type: ' + info.token_type);
 
-        let client = new faye.Client(instance_url + '/cometd/40.0/');
+        let client = new faye.Client( instance_url + '/cometd/40.0/' );
 
         // adding the OAuth token header
-        client.setHeader('Authorization', 'OAuth ' + access_token);
-        client.subscribe('/event/Verification_Check_Change__e', function(message) {
-            console.log('Got a message: \n' + JSON.stringify(message));
-            console.log('schema: \n' + JSON.stringify(message.schema));
-            console.log('payload: \n' + JSON.stringify(message.payload));
+        client.setHeader( 'Authorization', 'OAuth ' + access_token );
+		client.subscribe( '/event/' + sf_event_name, function(message) {
+        //client.subscribe('/event/File_Uploaded_Event__e', function(message) {
+            console.log( 'Got a message: \n' + JSON.stringify( message ) );
+            //console.log('schema: \n' + JSON.stringify(message.schema));
+
+			// this is a string
+			//let changed_data = JSON.parse(unescapeJs(message.payload.Changed_Data_JSON__c));
+            //console.log('payload: \n' + JSON.stringify( changed_data, null, 3 ) );
         });
 
 		// this will just confirm that the the subscribtion is active
-        client.callback(function() {
-            console.log('Subscribed...' );
+        client.callback( function() {
+            console.log( 'Subscribed...' );
         });
 
         // log that upstream subscription encounters error
-        client.errback(function(error) {
-            console.error("ERROR ON subscription Attempt: " + error.message );
+        client.errback( function( error ) {
+            console.error( "ERROR ON subscription Attempt: " + error.message );
         });
     } else {
-        console.log('Failed to get an OAuth token... :(');
-        console.log('Response:\n' + response);
+        console.log( 'Failed to get an OAuth token... :(' );
+        console.log( 'Response:\n' + response );
     }
 };
 
-// make the request
+
+var url_post = {
+    url: config.oauth_url,
+    form: {
+        client_id: config.oauth_settings.client_id,
+        client_secret: config.oauth_settings.client_secret,
+        username: config.oauth_settings.user_name,
+        password: config.oauth_settings.password,
+        grant_type: config.oauth_settings.grant_type
+    }
+};
 request.post(url_post, getOAuthAndListenForEvents);
